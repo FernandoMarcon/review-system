a5da8f0d1783e251c2c6648af8399b9b96b8b641
Fernando Marcon Passos <fmarcon@alumni.usp.br>
2025-09-03 09:29:40 -0300

    added pipeline

--- src/find_yesterdays_notes.sh
+++ src/find_yesterdays_notes.sh
@@ -2,11 +2,7 @@
 
 # A script to find Markdown files modified yesterday.
 
-# Directory to search in. Defaults to the current directory "." if no argument is provided.
-# SEARCH_DIR="${1:-.}"
-SEARCH_DIR="${HOME}/Vault"
-
-echo "Searching for Markdown files (.md, .markdown) in '$SEARCH_DIR' modified yesterday..."
+SEARCH_DIR="${1:-${HOME}/Vault}"
 
 # find: the command to search for files
 # $SEARCH_DIR: the directory to start the search from
@@ -14,4 +10,8 @@ echo "Searching for Markdown files (.md, .markdown) in '$SEARCH_DIR' modified ye
 # \( -iname "*.md" -o -iname "*.markdown" \): find files with names ending in .md or .markdown (case-insensitive)
 # -newermt "yesterday": find files modified at or after the start of yesterday
 # -not -newermt "today": exclude files modified at or after the start of today
-find "$SEARCH_DIR" -type f \( -iname "*.md" -o -iname "*.markdown" \) -newermt "yesterday" -not -newermt "today"
+find "$SEARCH_DIR" \
+    -type f \
+    \( -iname "*.md" -o -iname "*.markdown" \) \
+    -newermt "yesterday" \
+    -not -newermt "today"
